186549e0f04d3fd5b74ea026b0d63915fef0f028
Ej-Storm <Saryn.inc@gmail.com>
2025-10-06 13:55:47 -0700

    Refactor: Revert to BrowserRouter for modern hosting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import AnimatedRoutes from './AnimatedRoutes';
