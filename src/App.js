845ecd68bd8c026257e301984adf54b96e2272bb
Ej-Storm <Saryn.inc@gmail.com>
2025-10-06 18:20:38 -0700

    fix: Correct nested src directory structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import AnimatedRoutes from './AnimatedRoutes';
+import logo from './logo.svg';
 import './App.css';
-
-import './NewComponents.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main>
-          <AnimatedRoutes />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
